cf16fd78c7f1ade50cac8a6ae46b61378fd2c709
Szilard Magyar <szilard.magyar@gmail.com>
2017-05-15 00:02:20 +0200

    pointers

--- 03_pointers/main.go
+++ 03_pointers/main.go
@@ -3,11 +3,13 @@ package main
 import "fmt"
 
 func zero(z *int) {
+  fmt.Println(z) // this memory address is the same like the one in func main
   *z = 0 // setting the value of the memory address to 0
 }
 
 func main() {
   x := 5
+  fmt.Println(&x) // this memory address is the same like the one in func main
   zero(&x) // memory address is passed by value
   fmt.Println(x) // x is 0
 }
