ca22870ec4bc1c67f69fb5d9b017175dac1eac55
Szilard Magyar <szilard.magyar@gmail.com>
2018-07-17 00:58:48 +0200

    pointers

---
+++ 04_pointers2/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func changeAddress() {
+
+}
+
+func changeValue() {
+
+}
+
+func main() {
+	a := []string{"leto", "paul", "teg"}
+	fmt.Println(a)
+	fmt.Println(&a)
+}
